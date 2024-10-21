d53092dd2153def50d57dca949a5f5a69f7f52f6
Ashley Chung <ashjchung@gmail.com>
2024-10-21 15:56:49 +0100

    query function formatting

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+
 def process_query(input):
     if input == "dinosaurs":
         return "Dinosaurs ruled the Earth 200 million years ago"
     return "Unknown"
+
 
 @app.route("/")
 def hello_world():
@@ -17,7 +19,9 @@ def submit():
     input_age = request.form.get("age")
     return render_template("hello.html", name=input_name, age=input_age)
 
+
 @app.route("/query", methods=["GET"])
 def query():
     q = request.args.get("q")
     return process_query(q)
+    
