03cc6de686604fc1e61c19bb3de23d9ed3462566
Ashley Chung <ashjchung@gmail.com>
2024-10-21 15:52:50 +0100

    query function

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+def process_query(input):
+    if input == "dinosaurs":
+        return "Dinosaurs ruled the Earth 200 million years ago"
+    return "Unknown"
 
 @app.route("/")
 def hello_world():
@@ -12,3 +16,8 @@ def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
     return render_template("hello.html", name=input_name, age=input_age)
+
+@app.route("/query", methods=["GET"])
+def query():
+    q = request.args.get("q")
+    return process_query(q)
