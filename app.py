a72ad3e205b06a168b18bebb73eb15838d83d37e
Ashley Chung <ashjchung@gmail.com>
2024-11-05 15:35:09 +0000

    add: user/local repo info

--- app.py
+++ app.py
@@ -52,13 +52,64 @@ def get_githubname():
     return render_template("github_index.html")
 
 
+def get_commit_info(repo_name):
+    commit_response = requests.get(f"https://api.github.com/repos/{repo_name}/commits")
+
+    if commit_response.status_code == 200:
+        commits = commit_response.json()
+
+        hash = commits[0]["sha"]
+        date = commits[0]["commit"]["author"]["date"]
+        author = commits[0]["commit"]["author"]["name"]
+        message = commits[0]["commit"]["message"]
+
+    return hash, date, author, message
+
+
+def get_user_info(username):
+    user_response = requests.get(f"https://api.github.com/users/{username}")
+
+    if user_response.status_code == 200:
+        user_info = user_response.json()
+
+        bio = user_info["bio"]
+        num_public_repos = user_info["public_repos"]
+        account_created = user_info["created_at"]
+        num_followers = user_info["followers"]
+        num_following = user_info["following"]
+
+    return bio, num_public_repos, account_created, num_followers, num_following
+
+
 @app.route("/github_submit", methods=["POST"])
 def github_submit():
     input_githubname = request.form.get("githubname")
-    response = requests.get("https://api.github.com/users/{}/repos".format(input_githubname))
+    response = requests.get(f"https://api.github.com/users/{input_githubname}/repos")
+    
+    bio, num_public_repos, account_created, num_followers, num_following = get_user_info(input_githubname)
+    user_info = {
+        "bio": bio,
+        "num_public_repos": num_public_repos,
+        "account_created": account_created,
+        "num_followers": num_followers,
+        "num_following": num_following
+    }
+
     repos_list = []
+
     if response.status_code == 200:
         repos = response.json() # data returned is a list of ‘repository’ entities
         for repo in repos:
-            repos_list.append(repo["full_name"])
-    return render_template("github_submit.html", name=input_githubname, repos=repos_list)
+            # repos_list.append(repo["full_name"])
+            hash, date, author, message = get_commit_info(repo["full_name"])
+            repo_info = {
+                "name": repo["full_name"],
+                "date": date,
+                "hash": hash,
+                "author": author,
+                "message": message,
+                "repo_created": repo["created_at"]
+            }
+            repos_list.append(repo_info)
+
+    return render_template("github_submit.html", name=input_githubname, repos=repos_list, user=user_info)
